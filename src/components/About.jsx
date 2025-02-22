0561cdecabe8226c9a9a8c30a4e918ff010d3c2b
Anthony Zheng <ant39844@gmail.com>
2025-02-22 03:29:07 -0600

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           className={`mt-4 ${darkMode ? 'text-text' : 'text-text-light'} text-[17px] max-w-3xl leading-[30px] font-light`}
         >
           I'm an aspiring software engineer interested in making software that will cheer up someones day. I am currently a student at the University of Minnesota Twin Cities, 
-          finishing up my Computer Science degree. Some hobbies of mine include playing basketball, video games and eating out (big back). Feel free to check out 
+          finishing up my Computer Science degree. Some hobbies of mine include playing basketball, video games and eating out. Feel free to check out 
           some of my projects and past work experience below! Also, here's my 
           <button
             className="text-[17 px] px-1"
